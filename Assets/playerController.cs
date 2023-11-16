0d6a5e5a3be579521397d3e89fe123f10b65df08
Bn-adams <126073556+Bn-adams@users.noreply.github.com>
2023-11-16 13:42:02 +0000

    +

--- Assets/playerController.cs
+++ Assets/playerController.cs
@@ -4,15 +4,25 @@ using UnityEngine;
 
 public class playerController : MonoBehaviour
 {
+    Rigidbody rb;
+    private int horizontalInput;
+    private int verticalInput;
+    private int MS = 3;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Movement();
+    }
+    public void Movement()
+    {
+        horizontalInput = Input.GetAxisRaw("Horizontal");
+
     }
 }
